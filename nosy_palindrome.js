58421c4d9ed345cd578287ed97c35b9d75c9bfeb
Michael Murry <38839723+MichaelMurry49@users.noreply.github.com>
2021-04-08 21:53:24 -0700

    Create nosy_palindrome.js

---
+++ nosy_palindrome.js
@@ -0,0 +1,30 @@
+class Solution {
+    solve(s) {
+        let start = 0;
+        let end = s.length-1;
+        let nums = "1234567890"
+
+        while(start <= end) {
+            if(nums.includes(s[start]) || s[start] !== s[start].toLowerCase()) {
+                start++;
+                continue;
+            }
+
+            if(nums.includes(s[end]) || s[end] !== s[end].toLowerCase()) {
+                end--;
+                continue;
+            }
+
+            if(s[start] === s[end]) {
+                start++;
+                end--;
+                continue;
+            }
+
+            return false;
+
+        } 
+
+        return true;
+    }
+}
