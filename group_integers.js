116d01e2cc18faf53c7cbad32783d39b81f3d3f8
Michael Murry <38839723+MichaelMurry49@users.noreply.github.com>
2021-06-15 22:29:30 -0700

    Create group_integers.js

---
+++ group_integers.js
@@ -0,0 +1,26 @@
+class Solution {
+    solve(nums) {
+        let counts = {};
+        let max = 0;
+
+        for(let num of nums) {
+            counts[num] ? counts[num]++ : counts[num] = 1;
+            if(counts[num] > max) max = counts[num];
+        }
+
+        let keys = Object.keys(counts);
+
+        let allWork = false;
+        for(let i = 2; i <= max; i++) {
+            for(let j = 0; j < keys.length; j++) {
+                if(counts[keys[j]] % i !== 0) break;
+                if(j === keys.length - 1) allWork = true;
+            }
+            if(allWork) return true;
+        }
+
+
+        // console.log(counts, max)
+        return false;
+    }
+}
