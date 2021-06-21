92eac882e52e545e15fe5295192bee15fa264600
Michael Murry <38839723+MichaelMurry49@users.noreply.github.com>
2021-06-20 22:24:34 -0700

    Create robinhood.js

---
+++ robinhood.js
@@ -0,0 +1,21 @@
+class Solution {
+    solve(n, e, o, t) {
+        let alt = "e";
+        let val = n;
+        let count = 0;
+
+        while(val < t) {
+            if(alt === "e") {
+                val += (val * (e/100))
+                alt = "o"
+            } else {
+                val += (val * (o/100))
+                alt = "e"
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+}
