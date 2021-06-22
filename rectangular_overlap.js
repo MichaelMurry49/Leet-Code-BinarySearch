0d5032d75ddcb5b5d42c8e6cbc9a27ef11da882b
Michael Murry <38839723+MichaelMurry49@users.noreply.github.com>
2021-06-21 22:18:28 -0700

    Create rectangular_overlap.js

---
+++ rectangular_overlap.js
@@ -0,0 +1,19 @@
+class Solution {
+    solve(rect0, rect1) {
+        let x0 = rect0[0];
+        let x1 = rect0[2];
+        let xa = rect1[0];
+        let xb = rect1[2];
+        let y0 = rect0[1];
+        let y1 = rect0[3];
+        let ya = rect1[1];
+        let yb = rect1[3];
+
+        // check if x1 fits range or x0 fits range
+        if((x0 > xa && x0 < xb) || (x1 > xa && x1 < xb) || (x0 <= xa && x1 >= xb)) {
+            // check y range
+            if((y0 > ya && y0 < yb) || (y1 > ya && y1 < yb) || (y0 <= ya && y1 >= yb)) return true;
+        }
+        return false;
+    }
+}
