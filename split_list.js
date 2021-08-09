d2dd976b9a1bd2c54acca9ff68f766829c7d5e2a
Michael Murry <38839723+MichaelMurry49@users.noreply.github.com>
2021-08-09 10:23:33 -0700

    Update split_list.js

--- split_list.js
+++ split_list.js
@@ -1,12 +1,31 @@
 class Solution {
     solve(nums) {
-        let min = Infinity;
-        while(nums.length > 1) {
-            let temp = nums.pop();
-            if(min > temp) min = temp;
-            // console.log(Math.max(...nums), min)
-            if(min > Math.max(...nums)) return true;
+        let forwardMax = [];
+        let backwardMin = [];
+
+        let fmax = nums[0];
+        let bmin = nums[nums.length-1];
+        
+        for(let i = 0; i < nums.length; i++) {
+            if(nums[i] > fmax) {
+                fmax = nums[i];
+            }
+
+            if(nums[nums.length-1-i] < bmin) {
+                bmin = nums[nums.length-1-i];
+            }
+
+            forwardMax.push(fmax);
+            backwardMin.push(bmin);
         }
+
+        backwardMin = backwardMin.reverse();
+
+        for(let i = 0; i < nums.length-1; i++) {
+            if(forwardMax[i] < backwardMin[i+1]) return true;
+        }
+
         return false;
+
     }
 }
