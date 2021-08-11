166a414a04dea12f8764ec3c83dc7b26bb680a4c
Michael Murry <38839723+MichaelMurry49@users.noreply.github.com>
2021-08-11 12:20:40 -0700

    Create removing_triple_successive_duplicates.js

---
+++ removing_triple_successive_duplicates.js
@@ -0,0 +1,27 @@
+class Solution {
+    solve(s) {
+        let cur = "";
+        let count = 0;
+        let moves = 0;
+
+        for(let i = 0; i < s.length; i++) {
+            if(s[i] === cur) {
+                count++
+            } else {
+                cur = s[i];
+                if(count >= 3) {
+                    let val = Math.floor(count/3)
+                    moves += val;
+                }
+                count = 1;
+            }
+        }
+
+        if(count >= 3) {
+            let val = Math.floor(count/3)
+            moves += val;
+        }
+
+        return moves;
+    }
+}
