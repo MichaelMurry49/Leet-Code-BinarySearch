c7d3a1e25cce2a2b3ed799e4eb9fe2f46e548d1c
Michael Murry <38839723+MichaelMurry49@users.noreply.github.com>
2021-09-09 22:28:21 -0700

    Update minimum-time-visiting-all-points.js

--- Java Script/Easy/minimum-time-visiting-all-points.js
+++ Java Script/Easy/minimum-time-visiting-all-points.js
@@ -19,5 +19,6 @@ var minTimeToVisitAllPoints = function (points)
         let w = Math.abs(points[i][0] - points[i + 1][0]);
         distance += h > w ? h : w;
     }
+    
     return distance;
 };
