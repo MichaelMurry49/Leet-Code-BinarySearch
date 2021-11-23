73d3936d57bf10de9430ea0493a65349c0e53c5f
Michael Murry <38839723+MichaelMurry49@users.noreply.github.com>
2021-11-22 23:01:40 -0800

    Update replace_all_questions_to_avoid_consecutive_repeating_characters.js

--- Java Script/Easy/replace_all_questions_to_avoid_consecutive_repeating_characters.js
+++ Java Script/Easy/replace_all_questions_to_avoid_consecutive_repeating_characters.js
@@ -3,16 +3,16 @@
  * @return {string}
  */
 var modifyString = function(s) {
-    s = s.split("")
+    s = s.split("");
     
     for(let i = 0; i < s.length; i++){
         if(s[i] === "?"){
             if(i > 0 && s[i-1] === "a"){
-                i < s.length-1 && s[i+1] === "b" ? s[i] = "c" : s[i] = "b"
+                i < s.length-1 && s[i+1] === "b" ? s[i] = "c" : s[i] = "b";
             } else if(i < s.length-1 && s[i+1] === "a") {
-                i > 0 && s[i-1] === "b" ? s[i] = "c" : s[i] = "b"
+                i > 0 && s[i-1] === "b" ? s[i] = "c" : s[i] = "b";
             } else {
-                s[i] = "a"
+                s[i] = "a";
             }
         }
     }
