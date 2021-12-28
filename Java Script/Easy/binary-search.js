2a26016f9f8812b4b2c37966491a92183b65e2a2
Michael Murry <38839723+MichaelMurry49@users.noreply.github.com>
2021-12-27 23:48:17 -0800

    Create binary-search.js

---
+++ Java Script/Easy/binary-search.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let start = 0;
+    let end = nums.length-1;
+    while(start <= end) {
+        let mid = Math.floor(((end-start+1)/2)) + start;
+        // console.log(mid, start, end)
+        if(nums[mid] === target) {
+            return mid;
+        }
+        else if(nums[mid] > target) {
+            end = mid-1;
+        }
+        else { 
+            start = mid+1;
+        }
+    }
+    return -1;
+};
